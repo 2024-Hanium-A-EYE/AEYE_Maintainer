f2a6553b87b411dce9749bb5aded1f654790ad47
yoonchulchung <yoonchul005@gmail.com>
2024-08-09 23:36:33 +0900

    AEYE MW Print Log

---
+++ AEYE_Netowork_Operator/mw/views/AEYE_save_log.py
@@ -0,0 +1,94 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework import status
+from .models import aeye_print_log_models
+from .serializers import aeye_print_log_serializers
+from colorama import Fore, Back, Style
+from datetime import datetime
+import requests
+from django.conf import settings
+
+
+def print_log(status, whoami, api, message) :
+    now = datetime.now()
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    if status == "active" :
+        print("\n-----------------------------------------\n"   + 
+              current_time + " [ " + whoami + " ] send to : " + Fore.LIGHTBLUE_EX + "[ " + api + " ]" +  
+              Fore.RESET + "\n" + Fore.GREEN + "[active] " +  message + Fore.RESET +
+              "\n-----------------------------------------")
+    elif status == "error" :
+        print("\n-----------------------------------------\n"   + 
+              current_time + " [ " + whoami + " ] send to : " + Fore.BLUE + "[ " + api + " ]" +  
+              Fore.RESET + "\n" + Fore.RED + "[error] " + Fore.RED + message + Fore.RESET +
+              "\n-----------------------------------------")
+
+server_url    = '127.0.0.1:2000/'
+hal_print_log = '/hal/print-log/'
+i_am_mw_pl = 'Maintainer MW - PL'
+
+class aeye_print_log_Viewsets(viewsets.ModelViewSet):
+    queryset=aeye_print_log_models.objects.all().order_by('id')
+    serializer_class=aeye_print_log_serializers
+
+    def create(self, request) :
+        serializer = aeye_print_log_serializers(data = request.data)
+
+        if serializer.is_valid() :
+            i_am_client        = serializer.validated_data.get('whoami')
+            message_client     = serializer.validated_data.get('message')
+            cllient_name_raw   = serializer.validated_data.get('cllient_name_raw')
+            client_message_raw = serializer.validated_data.get('client_message_raw')
+
+            if settings.DEBUG:
+                print_log('active', i_am_client, i_am_mw_pl, "Received Data Successfully!")
+
+            response_server = request_print_log(cllient_name_raw, client_message_raw)
+            
+            return response_server
+        else:
+            message="Received Invalid data : ".format(serializer.errors)
+            data={
+                'whoami' : i_am_mw_pl,
+                'message': message
+            }
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+
+
+def request_print_log(message_client : str, name_client : str)->Response:
+    
+    if settings.DEBUG:
+        message="Request Print Log to : {}{}".format()
+        print_log("active", i_am_mw_pl, i_am_mw_pl, message)
+
+    message="Request Print Log"
+    data={
+        'whoami'             : i_am_mw_pl,
+        'message'            : message,
+        'client_name_raw'    : name_client,
+        'client_message_raw' : message_client
+    }
+
+    url='{}{}'.format(server_url, hal_print_log)
+    response_server=requests.post(url, data=data)
+
+    if response_server.status_code==200:
+        
+        if settings.DEBUG:
+            message="Received From Server Well from : {}{}".format(server_url, hal_print_log)
+            print_log("active", i_am_mw_pl, i_am_mw_pl, message)
+        
+        
+        return response_server
+    else:
+        message="Failed to receive Data from : {}{}".format(server_url, hal_print_log)
+        data={
+            'whoami' : i_am_mw_pl,
+            'message': message
+        }
+        
+        return Response(data, status=status.HTTP_400_BAD_REQUEST)
+
+
--- AEYE_Netowork_Operator/mw/views/models.py
+++ AEYE_Netowork_Operator/mw/views/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class aeye_print_log_models (models.Model):
+    whoami             = models.CharField(max_length=20)
+    message            = models.CharField(max_length=20)
+    client_name_raw    = models.CharField(max_length=20)
+    client_message_raw = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name

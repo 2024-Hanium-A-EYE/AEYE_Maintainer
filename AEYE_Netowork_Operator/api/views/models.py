cc3bb5bd7364d1b1ed022306f523004e0621584f
yoonchulchung <yoonchul005@gmail.com>
2024-08-09 23:12:36 +0900

    added AEYE MNO

--- AEYE_Netowork_Operator/api/views/models.py
+++ AEYE_Netowork_Operator/api/views/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class aeye_mno_models (models.Model):
+    whoami    = models.CharField(max_length=20)
+    message   = models.CharField(max_length=20)
+    operation = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
